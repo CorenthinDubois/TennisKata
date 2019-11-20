0d7a46c3d5123d5978e9342d3baa4ad8922b2c09
Dubois Corenthin <duboiscorenthin@gmail.com>
2019-11-20 21:47:45 +0100

    Add comments for methods

--- TennisKata/src/com/corenthin/tennis/Game.java
+++ TennisKata/src/com/corenthin/tennis/Game.java
@@ -12,6 +12,11 @@ public class Game {
         this.player2 = player2;
     }
 
+	/**
+     * Method to display the game score
+     *
+     * @return current game score
+     */
     public String getScore() {
         if (this.hasTennisGameWinner()) {
             return getLeadingPlayer().getName() + " wins the game";
@@ -25,25 +30,50 @@ public class Game {
         return player1.getScoreDescription() + ", " + player2.getScoreDescription();
     }     
     
+	/**
+	 * Method to find the leading player
+	 *
+	 * @return Player
+	 */
     public Player getLeadingPlayer() {
         return (player1.getGameScore() > player2.getGameScore()) ? player1 : player2;
     }
     
+	/**
+     * Method to find if there is a winner.
+     *
+     * @return boolean
+     */
 	private boolean hasTennisGameWinner() {
 		int scoreDiff = this.scoreDiff();
 		return scoreDiff >= 2
 				&& (this.player1.getGameScore() >= 4 || this.player2.getGameScore() >= 4);
 	}
 	
+	/**
+     * Method to find if there is currently a deuce.
+     *
+     * @return boolean
+     */
 	private boolean isDeuce() {
 		return this.player1.getGameScore() >= 3 && this.player2.getGameScore() >= 3 && this.player1.getGameScore() == this.player2.getGameScore();
 	}
 	
+	/**
+     * Method to find if the player has the advantage on the other.
+     *
+     * @return boolean
+     */
 	private boolean hasAdvantage(Player player, Player adversary) {
 		int scoreDiff = this.scoreDiff();
 		return player.getGameScore() >= 4 && adversary.getGameScore() >= 3 && scoreDiff == 1;
 	}
 	
+	/**
+     * Method to find the score difference between the players.
+     *
+     * @return integer
+     */
 	private int scoreDiff() {
 		int scoreDiff = Math.abs(this.player1.getGameScore()
 				- this.player2.getGameScore());
