feaf0fadda8e235f9f8c656a56554ada1684be5d
Dubois Corenthin <duboiscorenthin@gmail.com>
2019-11-20 20:42:51 +0100

    Initial Commit

---
+++ TennisKata/src/com/corenthin/tests/GameTest.java
@@ -0,0 +1,27 @@
+package com.corenthin.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.*;
+
+import com.corenthin.tennis.Game;
+import com.corenthin.tennis.Player;
+
+public class GameTest {
+
+    Player player1;
+    Player player2;
+    Game game;
+
+    @Before
+    public void beforeGameTest() {
+    	player1 = new Player("player1");
+    	player2 = new Player("player2");
+        game = new Game(player1, player2);
+    }
+
+    @Test
+    public void scoreDescriptionForNewGame() {
+        assertEquals("love, love", game.getScore());
+    }
+}
